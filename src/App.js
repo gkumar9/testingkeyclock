645e8f30f7cf1f60eb147bb9d6bbfd74ed7bcd16
gkumar9 <gaurav9904@gmail.com>
2019-07-26 13:57:12 +0530

    working test example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+class App extends React.Component {
+  componentDidMount() {
+    // console.log("App mounting")
+   
+      axios({
+        url:
+          "http://staging.clarolabs.in:7060/clarosupport/farmerinfo/farmer/schema/",
+        method: "POST",
+        data: {
+          temp: "temp"
+        },
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(res => {
+          console.log("response from api:", res.data);
+        })
+        .catch(e => {
+          console.log("error:", e);
+        });
+    
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { HashRouter, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Keycloak from "keycloak-js";
+import axios from "axios";
+export const history = createBrowserHistory({
+  basename: process.env.PUBLIC_URL
+});
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let app = (
+  <HashRouter basename={"/"}>
+    <div>
+      <Route exact path="/" component={App} />
+    </div>
+  </HashRouter>
+);
+
+const kc = new Keycloak({
+  realm: "test",
+  url: "http://18.216.208.195:8080/auth/",
+  "ssl-required": "none",
+  resource: "test-app",
+  "public-client": true,
+  "verify-token-audience": true,
+  "use-resource-role-mappings": true,
+  "confidential-port": 0,
+  "clientId": "test-app",
+  // "clientSecret":"",
+  // "enable-cors": true
+});
+kc.init({ onLoad: "login-required" }).success( authenticated => {
+  console.log(authenticated,kc)
+  if(authenticated){
+    console.log(kc)
+    ReactDOM.render(app,
+      document.getElementById("root"));
+  }
+})
+.error((e)=>{
+  console.log(e)
+})
+
+// console.log(kc)
+axios.interceptors.request.use(config =>{
+  config.headers.Authorization = "Bearer " + kc.token;
+  console.log(config)
+  return Promise.resolve(config);
+}
+  
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
